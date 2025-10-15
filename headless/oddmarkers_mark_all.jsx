88f4aeed75cef630ad13ea5005bba763b820c733
Zcounts <zacounts@gmail.com>
2025-10-15 08:57:58 -0500

    Update oddmarkers_mark_all.jsx

--- headless/oddmarkers_mark_all.jsx
+++ headless/oddmarkers_mark_all.jsx
@@ -5,16 +5,3 @@
   var msg = runCore(false, s);
   app.setSDKEventMessage("Odd Markers: " + msg, "info"); // non-blocking toast
 })();
-```
-
----
-
-## headless/oddmarkers_mark_selected.jsx
-```javascript
-/* Headless runner: Mark Selected (assign a keyboard shortcut in Premiere) */
-#include "../host.jsx"
-(function(){
-  var s = JSON.parse(OM_readSettings());
-  var msg = runCore(true, s);
-  app.setSDKEventMessage("Odd Markers: " + msg, "info");
-})();
