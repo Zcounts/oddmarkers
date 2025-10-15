39bd1c26ee182d50a00e2411ebd0e1e45cf71663
Zcounts <zacounts@gmail.com>
2025-10-15 09:28:00 -0500

    Update and rename headless/oddmarkers_mark_selected.jsx to oddmarkers_mark_selected.jsx

--- oddmarkers_mark_selected.jsx
+++ oddmarkers_mark_selected.jsx
@@ -1,5 +1,5 @@
 /* Headless runner: Mark Selected (assign a keyboard shortcut in Premiere) */
-#include "../host.jsx"
+#include "host.jsx"
 (function(){
   var s = JSON.parse(OM_readSettings());
   var msg = runCore(true, s);
